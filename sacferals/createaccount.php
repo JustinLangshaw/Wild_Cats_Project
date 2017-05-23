9a52f504a7d1f798ed077c4aebfc0ed2950e4b90
Justin Langshaw <justinlangshaw@yahoo.com>
2017-05-23 12:32:12 -0700

    re enter password

--- sacferals/createaccount.php
+++ sacferals/createaccount.php
@@ -23,6 +23,7 @@
 		<fieldset><legend>Create Account</legend>
 		<label><input type='text' name='username'>User Name</label>
 		<label><input type='password' name='password'>Password</label>
+		<label><input type='password' name='repassword'>Re-Enter Password</label>
 		<label><input type='text' name='email'>Email</label>
 		<label><input type='submit' name='register' value='Register'></label>
 		</fieldset>
@@ -37,41 +38,48 @@ if(isset($_POST['register'])) //this processes after user submits data.
 	$username = $_POST['username'];
 	$email = $_POST['email'];
 	$password = $_POST['password'];
+	$repassword = $_POST['repassword'];
 	
 	$re = "/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/";
 	$reEmail = "/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/";
-	print "hi";
+	
 	//if user passes re test
-	if(true)//( preg_match($re, $username) && preg_match($reEmail, $email) )
-	{	//display current table
-		$querycheck = "select * from SacFeralsUsers where username='$username' or email='$email'";
-		$resultcheck = mysqli_query($link, $querycheck); //link query to database
-		
-		if(mysqli_num_rows($resultcheck) == 0)// test if query does "nothing", and thus has no records
-		{//if not, record doesn't exist so you can process the insert query
-			$query = "insert into SacFeralsUsers values('', '$username', '$email', '$password', '2')";
-			mysqli_query($link, $query); //link query to database
-			print "Account Created"; // print confirmation
-			
-			
-		}
-		else
-		{
-			
-			print "That record already exists!";
-		}
+	if($password != $repassword)
+	{
+		print "error: passwords do not match";
 	}
 	else
 	{
-		print "You did not fill out the form correctly!";
+		if(true)//( preg_match($re, $username) && preg_match($reEmail, $email) )
+		{	//display current table
+			$querycheck = "select * from SacFeralsUsers where username='$username' or email='$email'";
+			$resultcheck = mysqli_query($link, $querycheck); //link query to database
+			
+			if(mysqli_num_rows($resultcheck) == 0)// test if query does "nothing", and thus has no records
+			{//if not, record doesn't exist so you can process the insert query
+				$query = "insert into SacFeralsUsers values('', '$username', '$email', '$password', '2')";
+				mysqli_query($link, $query); //link query to database
+				print "Account Created"; // print confirmation
+				
+				
+			}
+			else
+			{
+				
+				print "That record already exists!";
+			}
+		}
+		else
+		{
+			print "You did not fill out the form correctly!";
+		}
 	}
-	
 } 
 	
 ?>
 	
 	
-	
+	<br>
 	<a href="userprofile.php">Back to login</a>
 
 </body>
