34b55e6d021099aa7c44402fb3efef803e165e22
Justin Langshaw <justinlangshaw@yahoo.com>
2017-05-24 00:16:40 -0700

    cleaned up + error message

--- sacferals/createaccount.php
+++ sacferals/createaccount.php
@@ -43,7 +43,6 @@ if(isset($_POST['register'])) //this processes after user submits data.
 	$re = "/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/";
 	$reEmail = "/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/";
 	
-	//if user passes re test
 	if($username == "" || $email == "" || $password == "" || $repassword == "")//doesn't execute? 
 	{
 		print "error: please fil out all fields";
@@ -54,28 +53,26 @@ if(isset($_POST['register'])) //this processes after user submits data.
 	}
 	else 
 	{
+		//if user passes re test
 		if( preg_match($re, $username) && preg_match($reEmail, $email) )
 		{	//display current table
 			$querycheck = "select * from SacFeralsUsers where username='$username' or email='$email'";
 			$resultcheck = mysqli_query($link, $querycheck); //link query to database
 			
 			if(mysqli_num_rows($resultcheck) == 0)// test if query does "nothing", and thus has no records
-			{//if not, record doesn't exist so you can process the insert query
+			{	//if not, record doesn't exist so you can process the insert query
 				$query = "insert into SacFeralsUsers values('', '$username', '$email', '$password', '2')";
 				mysqli_query($link, $query); //link query to database
-				print "Account Created"; // print confirmation
-				
-				
+				print "Account Created"; // print confirmation	
 			}
 			else
 			{
-				
 				print "error: That account name or email already exists";
 			}
 		}
 		else
 		{
-			print "error: Please use no special characters when creating username";
+			print "error: Please use no special characters when creating username and make sure your email is valid.";
 		}
 	}
 } 
