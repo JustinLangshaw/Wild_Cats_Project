bcec095d6b8ca4c4342d84e5c0f1ac2a3024f428
vitadub <vitalinadub@gmail.com>
2017-10-25 16:25:31 -0700

    fixed city problem (#61)
    * added .txt extention to Database_JobTables
    
    * tried to remove commas from $typeofworkstring
    
    * edit
    
    * added files back to forlder
    
    * added alice's part
    
    * xxx
    
    * Delete Database_JobTables.txt
    
    * comma strip
    
    * updated login page
    
    not sure if it'll show up correctly because I couldn't get to run in on my computer... I tested it by changing it to html instead of php..
    
    * minor change to login
    
    * merge problems..
    
    * userprofile.css small modification
    
    * create.account ext rename
    
    * updated createaccount.php
    
    * error msg box added
    
    * tooltip updates
    
    * buttons for "text ok" added to forms
    
    * volunteerform
    
    * volunteerform textok
    
    * removed <<<<<HEAD
    
    * small change so add 1 to db if textok is checked
    
    * Rename Database_JobTables.txt to Database_JobTables
    
    * a
    
    * Rename Database_JobTables to Database_JobTables.txt
    
    * updateprofile.php
    
    * able to submit update to profile
    
    * auto populate city&county by zip
    
    * restrict zip to CA
    
    * Update reportform.php
    
    * Update reportform.php
    
    * fixed city problem
    
    * city fix
    
    * Update reportform.php

--- sacferals/reportform.php
+++ sacferals/reportform.php
@@ -507,7 +507,10 @@ $(document).ready(function(){
 						$select.attr('name','city');
 						$('#city_wrap').html($select);
 					}
-					else $('#city').val(city);
+					else {
+						$('#city_wrap').html('<input type="text" name="city" id="city">');
+						$('#city').val(city);
+					}
 					$('#county').val(county);
 				}
 				else{
