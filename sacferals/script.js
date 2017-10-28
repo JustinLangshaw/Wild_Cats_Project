9e23d04123763fe193aa360911d8e8d3e4fc5d1f
vitadub <vitalinadub@gmail.com>
2017-10-28 00:24:00 -0700

    Update script.js

--- sacferals/script.js
+++ sacferals/script.js
@@ -15,35 +15,30 @@ $(document).ready(function ()
 });
 
 
-function displayForm(c) 
-{
-    if (c.value == "catcolony") 
-	{    
+function displayForm(c) {
+    if (c.value == "catcolony") {    
         jQuery('#catcolony1').fadeIn('fast');
         jQuery('#intervention1').fadeOut('fast'); 
     }
-    if (c.value == "intervention") 
-	{
+    if (c.value == "intervention") {
          jQuery('#intervention1').fadeIn('fast');
          jQuery('#catcolony1').fadeOut('fast'); 
     }
 
-	if (c.value == "Yes") 
-	{
-		if(c.name == "caregiver[]")
-			$("#feederID").fadeOut('fast');
-		if(c.name == "recentlyinjured[]")
-			jQuery('#recentlyinjuredID').fadeIn('fast');
-		if(c.name == "othersworking[]")
-			jQuery('#othersworkingID').fadeIn('fast');
+    if (c.value == "Yes") {
+	if(c.name == "caregiver[]")
+	    $("#feederID").fadeOut('fast');
+	if(c.name == "recentlyinjured[]")
+	    jQuery('#recentlyinjuredID').fadeIn('fast');
+	if(c.name == "othersworking[]")
+	    jQuery('#othersworkingID').fadeIn('fast');
     }
-	if (c.value == "No") 
-	{	
-		if(c.name == "caregiver[]")
-			$("#feederID").fadeIn('fast');
-		if(c.name == "recentlyinjured[]")
-			jQuery('#recentlyinjuredID').fadeOut('fast');
-		if(c.name == "othersworking[]")
-			jQuery('#othersworkingID').fadeOut('fast'); 
+    if (c.value == "No") {	
+	if(c.name == "caregiver[]")
+	    $("#feederID").fadeIn('fast');
+	if(c.name == "recentlyinjured[]")
+	    jQuery('#recentlyinjuredID').fadeOut('fast');
+	if(c.name == "othersworking[]")
+	    jQuery('#othersworkingID').fadeOut('fast'); 
     }
 };
