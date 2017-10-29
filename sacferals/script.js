6702afc00c9cd80978d0e22f626f25f31dea7f1d
vitadub <vitalinadub@gmail.com>
2017-10-28 21:19:13 -0700

    removed report type (#68)
    * added .txt extention to Database_JobTables
    
    * tried to remove commas from $typeofworkstring
    
    * edit
    
    * added files back to forlder
    
    * added alice's part
    
    * xxx
    
    * Delete Database_JobTables.txt
    
    * comma strip
    
    * updated login page
    
    not sure if it'll show up correctly because I couldn't get to run in on my computer... I tested it by changing it to html instead of php..
    
    * minor change to login
    
    * merge problems..
    
    * userprofile.css small modification
    
    * create.account ext rename
    
    * updated createaccount.php
    
    * error msg box added
    
    * tooltip updates
    
    * buttons for "text ok" added to forms
    
    * volunteerform
    
    * volunteerform textok
    
    * removed <<<<<HEAD
    
    * small change so add 1 to db if textok is checked
    
    * Rename Database_JobTables.txt to Database_JobTables
    
    * a
    
    * Rename Database_JobTables to Database_JobTables.txt
    
    * updateprofile.php
    
    * able to submit update to profile
    
    * auto populate city&county by zip
    
    * restrict zip to CA
    
    * Update reportform.php
    
    * Update reportform.php
    
    * fixed city problem
    
    * city fix
    
    * Update reportform.php
    
    * display form on checkbox
    
    * email tooltip
    
    * minor form updates
    
    * caregiver/feeder update
    
    * Update script.js
    
    * form update #2

--- sacferals/script.js
+++ sacferals/script.js
@@ -1,44 +1,28 @@
-$(document).ready(function () 
-{
-    $('.checkdisplay').change(function () 
-	{
-        if (this.checked) 
-		{ 
+$(document).ready(function () {
+    $('.checkdisplay').change(function () {
+        if (this.checked)
           $('.todisplay').fadeIn('fast'); 
-        }
-        else 
-		{
+        else
           $('.todisplay').fadeOut('fast'); 
-        }
-
     });
 });
 
 
 function displayForm(c) {
-    if (c.value == "catcolony") {    
-        jQuery('#catcolony1').fadeIn('fast');
-        jQuery('#intervention1').fadeOut('fast'); 
-    }
-    if (c.value == "intervention") {
-         jQuery('#intervention1').fadeIn('fast');
-         jQuery('#catcolony1').fadeOut('fast'); 
-    }
-
     if (c.value == "Yes") {
-	if(c.name == "caregiver[]")
-	    $("#feederID").fadeOut('fast');
-	if(c.name == "recentlyinjured[]")
-	    jQuery('#recentlyinjuredID').fadeIn('fast');
-	if(c.name == "othersworking[]")
-	    jQuery('#othersworkingID').fadeIn('fast');
+        if(c.name == "caregiver[]")
+            $("#feederID").fadeOut('fast');
+        if(c.name == "recentlyinjured[]")
+            $('#recentlyinjuredID').fadeIn('fast');
+        if(c.name == "othersworking[]")
+            $('#othersworkingID').fadeIn('fast');
     }
-    if (c.value == "No") {	
-	if(c.name == "caregiver[]")
-	    $("#feederID").fadeIn('fast');
-	if(c.name == "recentlyinjured[]")
-	    jQuery('#recentlyinjuredID').fadeOut('fast');
-	if(c.name == "othersworking[]")
-	    jQuery('#othersworkingID').fadeOut('fast'); 
+    if (c.value == "No") {  
+        if(c.name == "caregiver[]")
+            $("#feederID").fadeIn('fast');
+        if(c.name == "recentlyinjured[]")
+            $('#recentlyinjuredID').fadeOut('fast');
+        if(c.name == "othersworking[]")
+            $('#othersworkingID').fadeOut('fast'); 
     }
 };
