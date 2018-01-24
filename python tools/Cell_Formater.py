f3dab3bc02d4446d909d718dff559ea006e60cab
Fplus1 <Fplus1@users.noreply.github.com>
2018-01-23 23:16:36 -0800

    Modify

--- python tools/Cell_Formater.py
+++ python tools/Cell_Formater.py
@@ -18,7 +18,7 @@ import sys
 import re
 
 #Declare file paths
-filename = "C:\Users\White Box\Desktop\Python\TestReport3.xls"
+filename = "E:\\Drive\\Fall 17\\CSc 191\\5.xls"
 sheet = "Open Reports"
 
 #Declare Variables
@@ -38,12 +38,12 @@ def savetoWorkbook(workbook, sheet, row, col, valuetosave, newComment):
 try:
     activeWorkBook = xlrd.open_workbook(filename)
 except Exception:
-    print"Failed to open workbook!"
+    print("Failed to open workbook!")
     sys.exit(1)
 try:
     activeSheet = activeWorkBook.sheet_by_name(sheet)
 except Exception:
-    print"Failed to open sheet!"
+    print("Failed to open sheet!")
     sys.exit(1)
 #Perform Filtering upon successful opening
 for x in range(1, 44):
@@ -54,31 +54,31 @@ for x in range(1, 44):
             if type(example) == float:
                 #Is it outside the acceptable range?
                 if 0 > example or example > 100:
-                    print x + 1, " | Fail: Out of Range, appending to comments"
+                    print(x + 1, " | Fail: Out of Range, appending to comments")
                     newComment = "<FLAG> No. Cats: (Out of Bounds) " + activeSheet.cell(x, 23).value
                     savetoWorkbook(filename, sheet, x, 15, example, newComment)
             #See if the cell is empty
             elif not any(activeSheet.cell(x, 15).value):
-                print x + 1, " | Corrected. Empty Field, Appended Null to Comments"
+                print(x + 1, " | Corrected. Empty Field, Appended Null to Comments")
                 totalAppends += 1
                 totalCorrections += 1
                 newComment = "<FLAG> No. Cats: (Null) " + example + "  " + activeSheet.cell(x, 23).value
                 savetoWorkbook(filename, sheet, x, 15, None, newComment)
             #See if it is a string of some kind
-            elif isinstance(example, basestring):
+            elif isinstance(example, str):
                 try: 
                     #Attempt to make it into a number and check range. E.g. five to 5 or fifteen to 15
                     example = w2n.word_to_num(str(example))
                     if 0 < example and example < 100:
                         totalCorrections += 1
-                        print x + 1, " | Corrected"
+                        print(x + 1, " | Corrected")
                         wb = copy(open_workbook(filename))
                         ws = wb.get_sheet(sheet)
                         ws.write(x, 15, example)
                         wb.save(filename)
                         savetoWorkbook(filename, sheet, x, 15, int(example), activeSheet.cell(x, 23).value)
                     else:
-                        print x + 1, " | Appending to comments"
+                        print(x + 1, " | Appending to comments")
                         totalAppends += 1
                         newComment = "<FLAG> No. Cats: " + example + "  " + activeSheet.cell(x, 23).value
                         savetoWorkbook(filename, sheet, x, 15, int(example), newComment)
@@ -88,20 +88,21 @@ for x in range(1, 44):
                     digits = re.findall(r'\d+', example)
                     if len(digits) == 0:
                     #Plain String Found, no digits. E.g. Unknown, Don't Know
-                        print x + 1, " |  No digits found. Appending and Correcting."
+                        print(x + 1, " |  No digits found. Appending and Correcting.")
                         totalAppends += 1
                         totalCorrections += 1
                         newComment = "<FLAG> No. Cats: " + example + "  " + activeSheet.cell(x, 23).value
                         savetoWorkbook(filename, sheet, x, 15, None, newComment)
                     elif len(digits) == 1:
                         totalCorrections += 1
-                        print x + 1, " | Corrected"
+                        print(x + 1, " | Corrected")
                         savetoWorkbook(filename, sheet, x, 15, int(digits[0]), activeSheet.cell(x, 23).value)
                     #Two Digits found
                     elif len(digits) == 2:
                         #Range Detected, average between the two and append to comment.
                         if any(re.findall(r'\-|to', example)):
-                            print x + 1, " | ", digits, " Two Numbers Found with: -, to. Might be range. Averaging and appending"
+                            print(x + 1, " | ", digits,
+                                  " Two Numbers Found with: -, to. Might be range. Averaging and appending")
                             average = int(digits[0]) + int(digits[1])
                             average /= 2
                             totalAppends += 1
@@ -110,21 +111,23 @@ for x in range(1, 44):
                             savetoWorkbook(filename, sheet, x, 15, average, newComment)
                         elif any(re.findall(r'\+|and|plus|includ', example)):
                             if any(re.findall(r'kitten|cub', example)):
-                                print x + 1, " | ", digits, " Two Numbers Found with: +, and, includes, including, kitten, kittens. Might be range. Averaging and appending"
+                                print(x + 1, " | ", digits,
+                                      " Two Numbers Found with: +, and, includes, including, kitten, kittens. Might be range. Averaging and appending")
                                 average = int(digits[0]) + int(digits[1])
                                 totalAppends += 1
                                 totalCorrections += 1
                                 newComment = "<FLAG> Kittens! No. Cats: " + digits[0] + " - " + digits[1] + "   "  + "  " + activeSheet.cell(x, 23).value
                                 savetoWorkbook(filename, sheet, x, 15, average, newComment)
                             else:
-                                print x + 1, " | ", digits, " Two Numbers Found with: +, and, includes, including. Might be range. Averaging and appending"
+                                print(x + 1, " | ", digits,
+                                      " Two Numbers Found with: +, and, includes, including. Might be range. Averaging and appending")
                                 average = int(digits[0]) + int(digits[1])
                                 totalAppends += 1
                                 totalCorrections += 1
                                 newComment = "<FLAG>No. Cats: " + digits[0] + " + " + digits[1] + "   "  + "  " + activeSheet.cell(x, 23).value
                                 savetoWorkbook(filename, sheet, x, 15, average, newComment)
                     else:
-                        print x + 1, " | ", digits, " Multiple digits found. Manual Inspection Required."
+                        print(x + 1, " | ", digits, " Multiple digits found. Manual Inspection Required.")
                         totalErrors += 1;
                         totalAppends += 1
                         totalCorrections += 1
@@ -136,14 +139,14 @@ for x in range(1, 44):
                         wb.save(filename)
         except Exception:
             #This gets thrown if there are ANY thrown exceptions regarding loading, saving, and performing operations with cells.
-            print x + 1, " | (!!) Exception thrown, requires manual inspection. Corrections might not have saved."
+            print(x + 1, " | (!!) Exception thrown, requires manual inspection. Corrections might not have saved.")
             totalErrors += 1;
-print "\n\nFiltering Complete!"
-print "Search for <FLAG> to see each comment that was altered during the run of this script."
-print "Total Records searched: ", totalSearched
-print "Total Corrections Made: ", totalCorrections       
-print "Total Comment Appends:  ", totalAppends
-print "Total Entries Untouched (Requires Manual Inspection): "   , totalErrors
+print("\n\nFiltering Complete!")
+print("Search for <FLAG> to see each comment that was altered during the run of this script.")
+print("Total Records searched: ", totalSearched)
+print("Total Corrections Made: ", totalCorrections)
+print("Total Comment Appends:  ", totalAppends)
+print("Total Entries Untouched (Requires Manual Inspection): ", totalErrors)
 
     
 
