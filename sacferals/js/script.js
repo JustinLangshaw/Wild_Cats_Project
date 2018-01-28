c418b0c61812ed25fe13b0893e77e64099783464
vitadub <vitalinadub@gmail.com>
2018-01-27 21:49:13 -0800

    query, report & volunteer forms updates

--- sacferals/js/script.js
+++ sacferals/js/script.js
@@ -9,39 +9,41 @@ $(document).ready(function () {
 
 
 function displayForm(c) {
-    if (c.value == "Yes") {
+    if (c.value == "Yes" || c.value == "other") {
         if(c.name == "caregiver[]")
             $("#feederID").fadeOut('fast');
-		if(c.name == "trapattempt[]")
+        if(c.name == "trapattempt[]")
             $("#trapdetails").fadeIn('fast');
-		if(c.name == "kittens[]")
-			$("#kittensdetails").fadeIn('fast');
+        if(c.name == "feedifreturned[]")
+            $("#notfeed").fadeOut('fast');
+        if(c.name == "kittens[]")
+            $("#kittensdetails").fadeIn('fast');
         if(c.name == "recentlyinjured[]")
             $('#recentlyinjuredID').fadeIn('fast');
         if(c.name == "othersworking[]")
             $('#othersworkingID').fadeIn('fast');
-		if(c.name=='addrdetails' && c.checked==true)
-			$('#addrdetails').fadeIn('fast');
-		else if(c.name=='addrdetails' && c.checked==false)
-			$('#addrdetails').fadeOut('fast');
-		if(c.name=='settingdetails' && c.checked==true)
-			$('#settingdetails').fadeIn('fast');
-		else if(c.name=='settingdetails' && c.checked==false)
-			$('#settingdetails').fadeOut('fast');
-		if(c.name=='typeofwork[]' && c.checked==true)
-			$('#othertasks').fadeIn('fast');
-		else if(c.name=='typeofwork[]' && c.checked==false)
-			$('#othertasks').fadeOut('fast');
+        if(c.name=='addrdetails' && c.checked==true)
+            $('#addrdetails').fadeIn('fast');
+        else if(c.name=='addrdetails' && c.checked==false)
+            $('#addrdetails').fadeOut('fast');
+        if(c.name=='settingdetails' && c.checked==true)
+            $('#settingdetails').fadeIn('fast');
+        else if(c.name=='settingdetails' && c.checked==false)
+            $('#settingdetails').fadeOut('fast');
+        if(c.name=='typeofwork[]' && c.checked==true)
+            $('#othertasks').fadeIn('fast');
+        else if(c.name=='typeofwork[]' && c.checked==false)
+            $('#othertasks').fadeOut('fast');
     }
     if (c.value == "No") {  
         if(c.name == "caregiver[]")
             $("#feederID").fadeIn('fast');
-		if(c.name == "trapattempt[]")
+        if(c.name == "trapattempt[]")
             $("#trapdetails").fadeOut('fast');
-		if(c.name == "feedifreturned[]")
-			$("#notfeed").fadeIn('fast');
-		if(c.name == "kittens[]")
-			$("#kittensdetails").fadeOut('fast');
+        if(c.name == "feedifreturned[]")
+            $("#notfeed").fadeIn('fast');
+        if(c.name == "kittens[]")
+            $("#kittensdetails").fadeOut('fast');
         if(c.name == "recentlyinjured[]")
             $('#recentlyinjuredID').fadeOut('fast');
         if(c.name == "othersworking[]")
